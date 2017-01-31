6aaf5c61e4ac5a151cc06a5312fcd3152fc290a9
Rohra <arohra@tmmc.com>
2017-01-30 22:38:13 -0800

    New Project
    Starting to commit

---
+++ client/app/scripts/controllers/movie-delete.js
@@ -0,0 +1,29 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name clientApp.controller:MovieDeleteCtrl
+ * @description
+ * # MovieDeleteCtrl
+ * Controller of the clientApp
+ */
+angular.module('clientApp')
+.controller('MovieDeleteCtrl', function (
+$scope,
+$routeParams,
+Movie,
+$location
+) {
+$scope.movie = Movie.one($routeParams.id).get().$object;
+$scope.deleteMovie = function() {
+  $scope.movie.remove().then(function() {
+    $location.path('/movies');
+  });
+};
+$scope.back = function() {
+  $location.path('/movie/' + $routeParams.id);
+};
+});
+CREATING A NAVIGATION TABS
+ADDING LINKS TO THE MOVIES PAGE.
+THANK YOU!
---
+++ server/models/index.js
@@ -0,0 +1,4 @@
+module.exports = {
+  movie: require('./Movie')
+  /*car:require(''./Car.js'), registry of all models in the system*/
+};
